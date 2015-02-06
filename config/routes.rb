699ba4ff177e2a138c7332a24acba683a7b2c13f
Carson Ng <ngcarson@gmail.com>
2015-02-07 01:59:00 +0800

    bundle install, all ready to push to heroku.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root "pages#home"
+  get "create" => "pages#create"
+  get "ideas" => "pages#ideas"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
