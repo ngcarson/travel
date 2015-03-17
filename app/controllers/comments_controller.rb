417e24e472842fa7ee2969c138aa823def6cb37a
Carson Ng <ngcarson@gmail.com>
2015-03-17 22:36:12 +0800

    Added DECS order & user email in comments reply.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,10 +2,20 @@ class CommentsController < ApplicationController
 	def create
 		@idea = Idea.find(params[:idea_id])
 		@comment = @idea.comments.create(params[:comment].permit(:comment))
+		@comment.user_id = current_user.id if current_user
+		@comment.save
+
 		if @comment.save
 			redirect_to idea_path(@idea)
 		else
 			render 'new'
 		end
+	end
+
+	def destroy
+		@idea = Idea.find(params[:idea_id])
+		@comment = @idea.comments.find(params[:id])
+		@comment.destroy
+		redirect_to idea_path(@idea)
 	end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :idea
   belongs_to :user
+
+  #validates :text, :presence => true, :length => { :minimum => 10 }
 end
