5949531da6069553493602dbcb14fb00f9fb0283
Carson Ng <ngcarson@gmail.com>
2015-03-24 00:43:18 +0800

    Debug: Double Flash Notice.

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -35,7 +35,7 @@ class IdeasController < ApplicationController
 
     respond_to do |format|
       if @idea.save
-        format.html { redirect_to @idea, notice: 'Idea was successfully created.' }
+        format.html { redirect_to @idea, alert: 'Idea was successfully created.' }
         format.json { render :show, status: :created, location: @idea }
       else
         format.html { render :new }
@@ -49,7 +49,7 @@ class IdeasController < ApplicationController
   def update
     respond_to do |format|
       if @idea.update(idea_params)
-        format.html { redirect_to @idea, notice: 'Idea was successfully updated.' }
+        format.html { redirect_to @idea, alert: 'Idea was successfully updated.' }
         format.json { render :show, status: :ok, location: @idea }
       else
         format.html { render :edit }
@@ -63,7 +63,7 @@ class IdeasController < ApplicationController
   def destroy
     @idea.destroy
     respond_to do |format|
-      format.html { redirect_to ideas_url, notice: 'Idea was successfully destroyed.' }
+      format.html { redirect_to ideas_url, alert: 'Idea was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -76,7 +76,7 @@ class IdeasController < ApplicationController
 
     def correct_user
       @idea = current_user.ideas.find_by(id: params[:id])
-      redirect_to ideas_path, notice: "You are not allowed to edit this." if @idea.nil?
+      redirect_to ideas_path, alert: "You are not allowed to edit this." if @idea.nil?
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
