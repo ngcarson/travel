76d006c5159b9d811ff3a93177c2539f68629436
Carson Ng <ngcarson@gmail.com>
2015-04-05 03:06:46 +0800

    Fixed google sign in bug.

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,13 +1,13 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController   
-  
-  skip_before_filter :verify_authenticity_token
-
-  def google_oauth2
+def google_oauth2
      
   @user = User.find_for_google_oauth2(request.env["omniauth.auth"], current_user)
 
-    @user.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
-      sign_in_and_redirect @user, :event => :authentication
+	  if @user.persisted?
+	    flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
+	    sign_in_and_redirect @user, :event => :authentication
+	  else
+	    session["devise.google_data"] = request.env["omniauth.auth"]
+	  end
   end
 end
